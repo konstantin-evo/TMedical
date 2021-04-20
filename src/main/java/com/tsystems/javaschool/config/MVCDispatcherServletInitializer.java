acbdcd87288810acd833b396205860accdf2a890
kpriluch <Konstantin.Priluchnyi@telekom.com>
2021-04-20 10:40:30 +0300

    Added MVCDispatcherServletInitializer Class

---
+++ src/main/java/com/tsystems/javaschool/config/MVCDispatcherServletInitializer.java
@@ -0,0 +1,46 @@
+package com.tsystems.javaschool.config;
+
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.FrameworkServlet;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.MultipartConfigElement;
+import javax.servlet.ServletRegistration;
+import java.io.File;
+
+public class MVCDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    private static final String LOCATION = new File(System.getProperty("java.io.tmpdir")).getAbsolutePath();
+    private static final long MAX_FILE_SIZE = 1024 * 1024 * 1L; //1MB
+    private static final long MAX_REQUEST_SIZE = 1024 * 1024 * 1L; //1MB
+    private static final int FILE_SIZE_THRESHOLD = 0;
+
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] { WebMVCConfig.class };
+    }
+
+    protected String[] getServletMappings() {
+        return new String[] { "/" };
+    }
+
+    @Override
+    protected void customizeRegistration(ServletRegistration.Dynamic registration) {
+        registration.setMultipartConfig(getMultipartConfigElement());
+    }
+
+    private MultipartConfigElement getMultipartConfigElement(){
+        return new MultipartConfigElement(LOCATION, MAX_FILE_SIZE, MAX_REQUEST_SIZE, FILE_SIZE_THRESHOLD);
+    }
+
+    @Override
+    protected FrameworkServlet createDispatcherServlet(WebApplicationContext servletAppContext) {
+        final DispatcherServlet dispatcherServlet = (DispatcherServlet) super.createDispatcherServlet(servletAppContext);
+        dispatcherServlet.setThrowExceptionIfNoHandlerFound(true);
+        return dispatcherServlet;
+    }
+}
