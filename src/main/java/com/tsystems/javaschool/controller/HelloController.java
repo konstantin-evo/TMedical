b28e90b8420ad797572a3743d7b342f6db7056a7
kpriluch <Konstantin.Priluchnyi@telekom.com>
2021-04-27 17:04:43 +0300

    Refactor HelloController to testing DAO

--- src/main/java/com/tsystems/javaschool/controller/HelloController.java
+++ src/main/java/com/tsystems/javaschool/controller/HelloController.java
@@ -1,18 +1,36 @@
 package com.tsystems.javaschool.controller;
 
+import com.tsystems.javaschool.dao.interfaces.PatientRepository;
+import com.tsystems.javaschool.dao.interfaces.UserRepository;
+import com.tsystems.javaschool.model.entity.Patient;
+import com.tsystems.javaschool.model.entity.UserEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/")
 public class HelloController {
+    @Autowired
+    UserRepository userRepository;
 
     @RequestMapping(method = RequestMethod.GET)
     public String printWelcome(ModelMap model) {
 
         return "login";
     }
+
+    @RequestMapping(value = "/test", method = RequestMethod.POST)
+    public List<UserEntity> test(@RequestBody UserEntity user) {
+        System.out.println(user.getSurname());
+        List<UserEntity> allPatient = userRepository.findAll();
+        return allPatient;
+    }
+
 
 }
