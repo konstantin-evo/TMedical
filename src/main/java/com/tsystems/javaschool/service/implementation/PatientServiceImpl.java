9a422f8a022f4700697a3fde6384c0f8412892a5
kpriluch <Konstantin.Priluchnyi@telekom.com>
2021-05-04 17:01:54 +0300

    Add PatientServiceImpl.java The build is deployment

---
+++ src/main/java/com/tsystems/javaschool/service/implementation/PatientServiceImpl.java
@@ -0,0 +1,17 @@
+package com.tsystems.javaschool.service.implementation;
+
+import com.tsystems.javaschool.dao.api.PatientRepository;
+import com.tsystems.javaschool.model.dto.PatientDto;
+import com.tsystems.javaschool.model.entity.Patient;
+import com.tsystems.javaschool.service.api.PatientService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class PatientServiceImpl extends AbstractServiceImpl<Patient, PatientRepository, PatientDto, Integer> implements PatientService {
+
+    @Autowired
+    public PatientServiceImpl(PatientRepository dao, ModelMapper mapper) {
+        super(dao, mapper, PatientDto.class, Patient.class);
+    }
+
+}
