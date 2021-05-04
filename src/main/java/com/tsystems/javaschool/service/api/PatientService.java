f12da3f295a57dc690557db62b7cf6db7ec06f16
kpriluch <Konstantin.Priluchnyi@telekom.com>
2021-05-04 17:01:40 +0300

    Add PatientService.java (interface) The build is deployment

---
+++ src/main/java/com/tsystems/javaschool/service/api/PatientService.java
@@ -0,0 +1,7 @@
+package com.tsystems.javaschool.service.api;
+
+import com.tsystems.javaschool.model.dto.PatientDto;
+import com.tsystems.javaschool.model.entity.Patient;
+
+public interface PatientService extends AbstractService<Patient, PatientDto, Integer> {
+}
