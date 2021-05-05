0c777139790818ef52651b7b5a80151b48f8a3bc
kpriluch <Konstantin.Priluchnyi@telekom.com>
2021-05-05 17:03:49 +0300

    Refactor InsuranceRepository.java The build is deployment

--- src/main/java/com/tsystems/javaschool/dao/api/InsuranceRepository.java
+++ src/main/java/com/tsystems/javaschool/dao/api/InsuranceRepository.java
@@ -1,17 +1,13 @@
 package com.tsystems.javaschool.dao.api;
 
 import com.tsystems.javaschool.model.entity.Insurance;
-
 import java.util.List;
 
-public interface InsuranceRepository {
+public interface InsuranceRepository extends AbstractRepository<Insurance, Integer> {
     /**
      * Find Insurance by surname
      * @param surname the patient
      * @return the list<Insurance> entities
      */
     List<Insurance> findInsuranceBySurname(String surname);
-
-
-    void add(Insurance insurance);
 }
