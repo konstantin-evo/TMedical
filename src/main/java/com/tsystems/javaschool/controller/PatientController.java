4d81ae711cdf0284d5bb519d28228ca20224d3df
kpriluch <Konstantin.Priluchnyi@telekom.com>
2021-05-05 16:20:31 +0300

    Refactor PatientController.java The build is deployment

--- src/main/java/com/tsystems/javaschool/controller/PatientController.java
+++ src/main/java/com/tsystems/javaschool/controller/PatientController.java
@@ -1,49 +1,46 @@
 package com.tsystems.javaschool.controller;
 
-import com.tsystems.javaschool.dao.api.UserRepository;
-import com.tsystems.javaschool.model.entity.UserEntity;
+import com.tsystems.javaschool.model.dto.PatientDto;
+import com.tsystems.javaschool.service.api.PatientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
-@RequestMapping("/patient")
+@RequestMapping(value = "/patient")
 public class PatientController {
 
-    private final UserRepository userRepository;
+    private final PatientService patientService;
 
     @Autowired
-    public PatientController(UserRepository userRepository) {
-        this.userRepository = userRepository;
+    public PatientController(PatientService patientService) {
+        this.patientService = patientService;
+    }
+    @ModelAttribute("patient")
+    public PatientDto patientDto() {
+        return new PatientDto();
+    }
+
+    @PostMapping(value = "/addPatient")
+    public String addPatient(@ModelAttribute("patient")@Valid PatientDto patient) {
+        patientService.save(patient);
+        return "redirect:patient/index";
+    }
+
+    @GetMapping("/add-patient")
+    public String newPatient() {
+        return "patient/add-patient";
     }
 
     @GetMapping("/index")
-    public String index(Model model) {
-        model.addAttribute("patient", userRepository.findAll());
+    public String index(@ModelAttribute PatientDto patient) {
+        //patientService.findAll();
         return "patient/index";
     }
 
-    @GetMapping("/{id}")
-    public String show(@PathVariable("id") int id, Model model) {
-        model.addAttribute("patient", userRepository.findById(id));
-        return "patient/show";
-    }
-
-//    @GetMapping("/{id}")
-//    public String doctor(@PathVariable("id") int id, Model model) {
-//        model.addAttribute("patient", userRepository.findPatientByDoctor(id));
-//        return "patient/doctor";
-//    }
-
-    @GetMapping("/new")
-    public String newPatient(@ModelAttribute("patient") UserEntity user) {
-        return "patient/new";
-    }
-
-    @PostMapping()
-    public String addPatient(@ModelAttribute("patient") UserEntity user) {
-        userRepository.save(user);
-        return "redirect:/patient";
-    }
 }
