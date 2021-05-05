16571133d6650e300b9010e69137fafee2ea2faf
kpriluch <Konstantin.Priluchnyi@telekom.com>
2021-05-05 20:01:55 +0300

    Refactor Gender.java enum (add displayValues) The build is deployment

--- src/main/java/com/tsystems/javaschool/model/entity/enums/Gender.java
+++ src/main/java/com/tsystems/javaschool/model/entity/enums/Gender.java
@@ -1,7 +1,17 @@
 package com.tsystems.javaschool.model.entity.enums;
 
 public enum Gender {
-    MALE,
-    FEMALE,
-    OTHER
+    MALE("Male"),
+    FEMALE("Female"),
+    OTHER("Other");
+
+    private final String displayValue;
+
+    private Gender(String displayValue) {
+        this.displayValue = displayValue;
+    }
+
+    public String getDisplayValue() {
+        return displayValue;
+    }
 }
