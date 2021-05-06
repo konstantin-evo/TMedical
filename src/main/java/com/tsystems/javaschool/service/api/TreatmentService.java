f1524ebc6d010290ae734396fbdf6f40d7799415
kpriluch <Konstantin.Priluchnyi@telekom.com>
2021-05-06 17:29:29 +0300

    Add TreatmentService.java The build is deployment

---
+++ src/main/java/com/tsystems/javaschool/service/api/TreatmentService.java
@@ -0,0 +1,7 @@
+package com.tsystems.javaschool.service.api;
+
+import com.tsystems.javaschool.model.dto.TreatmentDto;
+import com.tsystems.javaschool.model.entity.Treatment;
+
+public interface TreatmentService extends AbstractService<Treatment, TreatmentDto, Integer> {
+}
