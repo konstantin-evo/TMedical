4f7af7672db5a6b693a429af8c163f03837b515b
kpriluch <Konstantin.Priluchnyi@telekom.com>
2021-05-06 11:52:05 +0300

    Add InsuranceService.java The build is deployment

---
+++ src/main/java/com/tsystems/javaschool/service/api/InsuranceService.java
@@ -0,0 +1,7 @@
+package com.tsystems.javaschool.service.api;
+
+import com.tsystems.javaschool.model.dto.InsuranceDto;
+import com.tsystems.javaschool.model.entity.Insurance;
+
+public interface InsuranceService extends AbstractService<Insurance, InsuranceDto, Integer> {
+}
