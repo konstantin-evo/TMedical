efb9dd87f9deceb14a2e92c87b41c894d774adea
kpriluch <Konstantin.Priluchnyi@telekom.com>
2021-05-06 17:01:01 +0300

    Refactor PatientDto.java (change InsuranceDto to List type) The build is deployment

--- src/main/java/com/tsystems/javaschool/model/dto/PatientDto.java
+++ src/main/java/com/tsystems/javaschool/model/dto/PatientDto.java
@@ -6,11 +6,12 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.io.Serializable;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
 public class PatientDto implements Serializable {
     private int id;
     private UserDto userDto;
-    private InsuranceDto insurance;
+    private List<InsuranceDto> insurances;
 }
