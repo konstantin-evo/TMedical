519b21b6b0c417a9284f57bedf857f2ba7e84765
kpriluch <Konstantin.Priluchnyi@telekom.com>
2021-05-07 15:00:31 +0300

    Refactor TreatmentController.java The build is deployment

--- src/main/java/com/tsystems/javaschool/controller/TreatmentController.java
+++ src/main/java/com/tsystems/javaschool/controller/TreatmentController.java
@@ -1,33 +1,33 @@
 package com.tsystems.javaschool.controller;
 
 import com.tsystems.javaschool.model.dto.PatientDto;
-import com.tsystems.javaschool.service.api.PatientService;
+import com.tsystems.javaschool.model.dto.TreatmentDto;
 import com.tsystems.javaschool.service.api.TreatmentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.List;
 
 @Controller
 @RequestMapping(value = "/treatment")
 public class TreatmentController {
 
     private final TreatmentService treatmentService;
-    private final PatientService patientService;
 
     @Autowired
-    public TreatmentController(TreatmentService treatmentService, PatientService patientService) {
+    public TreatmentController(TreatmentService treatmentService) {
         this.treatmentService = treatmentService;
-        this.patientService = patientService;
     }
 
-    @GetMapping("/index")
-    public String index(Model model) {
-        List<PatientDto> patientList = patientService.findAll();
-        model.addAttribute("patientList", patientList);
-        return "treatment/index";
+    @ModelAttribute("treatment")
+    public TreatmentDto treatmentDto() {
+        return new TreatmentDto();
+    }
+
+    @GetMapping("/all")
+    public String index() {
+        return "treatment/all-treatments";
     }
 }
