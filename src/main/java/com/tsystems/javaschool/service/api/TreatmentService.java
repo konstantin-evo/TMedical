33540194359ffa4b8d44dd0f74ba268a7ea7f3a3
kpriluch <Konstantin.Priluchnyi@telekom.com>
2021-05-12 17:54:03 +0300

    Refactor TreatmentService.java (added "List<TreatmentDto> findByPatientId(int id)" method) The build is deployment

--- src/main/java/com/tsystems/javaschool/service/api/TreatmentService.java
+++ src/main/java/com/tsystems/javaschool/service/api/TreatmentService.java
@@ -3,5 +3,10 @@ package com.tsystems.javaschool.service.api;
 import com.tsystems.javaschool.model.dto.TreatmentDto;
 import com.tsystems.javaschool.model.entity.Treatment;
 
+import java.util.List;
+
 public interface TreatmentService extends AbstractService<Treatment, TreatmentDto, Integer> {
+
+    List<TreatmentDto> findByPatientId(int id);
+
 }
