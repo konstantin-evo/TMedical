8c1f4330810a83a5329aec655faf7de67232e189
kpriluch <Konstantin.Priluchnyi@telekom.com>
2021-05-12 17:41:38 +0300

    Refactor JPAConfig.java (added hibernateProperties related to UTF-8) The build is deployment

--- src/main/java/com/tsystems/javaschool/config/JPAConfig.java
+++ src/main/java/com/tsystems/javaschool/config/JPAConfig.java
@@ -6,15 +6,9 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
-import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.JpaVendorAdapter;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
@@ -66,7 +60,9 @@ public class JPAConfig {
         final Properties hibernateProperties = new Properties();
         hibernateProperties.setProperty("hibernate.hbm2ddl.auto", environment.getRequiredProperty("hibernate.hbm2ddl.auto"));
         hibernateProperties.setProperty("hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
-
+        hibernateProperties.setProperty("hibernate.connection.CharSet", "utf-8");
+        hibernateProperties.setProperty("hibernate.connection.useUnicode", "true");
+        hibernateProperties.setProperty("hibernate.connection.characterEncoding", "utf-8");
         return hibernateProperties;
     }
 
