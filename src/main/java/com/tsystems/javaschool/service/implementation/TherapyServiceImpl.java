2af237df316af9e4c8073bae0cd6ea4a92cf59e3
kpriluch <Konstantin.Priluchnyi@telekom.com>
2021-05-17 17:05:52 +0300

    Add convertToDto method to TherapyDto.java The build is deployment

--- src/main/java/com/tsystems/javaschool/service/implementation/TherapyServiceImpl.java
+++ src/main/java/com/tsystems/javaschool/service/implementation/TherapyServiceImpl.java
@@ -3,6 +3,7 @@ package com.tsystems.javaschool.service.implementation;
 import com.tsystems.javaschool.dao.api.TherapyRepository;
 import com.tsystems.javaschool.model.dto.TherapyDto;
 import com.tsystems.javaschool.model.entity.Therapy;
+import com.tsystems.javaschool.model.entity.enums.TreatmentType;
 import com.tsystems.javaschool.service.api.TherapyService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,5 +13,18 @@ public class TherapyServiceImpl extends AbstractServiceImpl<Therapy, TherapyRepo
     @Autowired
     public TherapyServiceImpl(TherapyRepository dao, ModelMapper mapper) {
         super(dao, mapper, TherapyDto.class, Therapy.class);
+    }
+
+    @Override
+    public TherapyDto convertToDTO(Therapy therapy) {
+        TherapyDto therapyDto = new TherapyDto();
+        therapyDto.setType(TreatmentType.valueOf(therapy.getMedication().getName()));
+        therapyDto.setPattern(therapy.getTimePattern());
+        therapyDto.setStartDate(String.valueOf(therapy.getStartDate()));
+        therapyDto.setEndDate(String.valueOf(therapy.getEndDate()));
+        therapyDto.setDose(therapy.getDose());
+        therapyDto.setNumberOfDays(therapy.getNumber());
+
+        return therapyDto;
     }
 }
