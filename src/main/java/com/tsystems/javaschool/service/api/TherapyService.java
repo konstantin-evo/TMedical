142b2ffffcbd85dc19d46881784b4cdd3260908e
kpriluch <Konstantin.Priluchnyi@telekom.com>
2021-05-17 15:18:26 +0300

    Add TherapyService.java The build is deployment

---
+++ src/main/java/com/tsystems/javaschool/service/api/TherapyService.java
@@ -0,0 +1,8 @@
+package com.tsystems.javaschool.service.api;
+
+import com.tsystems.javaschool.model.dto.TherapyDto;
+import com.tsystems.javaschool.model.entity.Therapy;
+
+public interface TherapyService extends AbstractService<Therapy, TherapyDto, Integer> {
+
+}
