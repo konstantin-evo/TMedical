d5e7c2df97c3c6c53a1c33a7fcb1a8913df8c751
kpriluch <Konstantin.Priluchnyi@telekom.com>
2021-05-17 13:38:16 +0300

    Add @PostMapping(value = "/add/{id}") to TreatmentController.java The build is deployment

--- src/main/java/com/tsystems/javaschool/controller/TreatmentController.java
+++ src/main/java/com/tsystems/javaschool/controller/TreatmentController.java
@@ -7,10 +7,7 @@ import com.tsystems.javaschool.service.api.TreatmentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -56,6 +53,12 @@ public class TreatmentController {
         PatientDto patient = patientService.findById(id);
         model.addAttribute("patient", patient);
         return "treatment/add-treatment";
+    }
+
+    @PostMapping(value = "/add/{id}")
+    public String addTreatment(@PathVariable("id") int id, @ModelAttribute("treatment") @Valid TreatmentDto treatment) {
+        treatmentService.save(treatment);
+        return "redirect:all";
     }
 
 }
