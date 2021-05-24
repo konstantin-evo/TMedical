9e1c2442a2e3af507c58729bf49bbbba986a7343
kpriluch <Konstantin.Priluchnyi@telekom.com>
2021-05-24 10:28:31 +0300

    Add MapperService.java The build is deployment

---
+++ src/main/java/com/tsystems/javaschool/service/api/MapperService.java
@@ -0,0 +1,21 @@
+package com.tsystems.javaschool.service.api;
+
+import com.tsystems.javaschool.model.dto.*;
+import com.tsystems.javaschool.model.entity.*;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface MapperService {
+        PatientDto convertToDto(Patient patientEntity);
+        Patient convertToEntity(PatientDto dto);
+
+        TreatmentDto convertToDto(Treatment treatment);
+        Treatment convertToEntity(TreatmentDto dto);
+
+        TherapyDto convertToDto(Therapy therapy);
+        Therapy convertToEntity(TherapyDto dto);
+
+        InsuranceDto convertToDto(Insurance insurance);
+
+        UserDto convertToDto(UserEntity userEntity);
+}
