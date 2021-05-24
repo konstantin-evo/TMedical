61a7f0886d9a99630c7a95ab7b8582f751b436f0
kpriluch <Konstantin.Priluchnyi@telekom.com>
2021-05-24 10:30:56 +0300

    Refactor PatientServiceImpl (delete AbstractService) The build is deployment

--- src/main/java/com/tsystems/javaschool/service/implementation/PatientServiceImpl.java
+++ src/main/java/com/tsystems/javaschool/service/implementation/PatientServiceImpl.java
@@ -1,100 +1,42 @@
 package com.tsystems.javaschool.service.implementation;
 
 import com.tsystems.javaschool.dao.api.PatientRepository;
-import com.tsystems.javaschool.model.dto.InsuranceDto;
 import com.tsystems.javaschool.model.dto.PatientDto;
-import com.tsystems.javaschool.model.dto.TreatmentDto;
-import com.tsystems.javaschool.model.dto.UserDto;
-import com.tsystems.javaschool.model.entity.Insurance;
-import com.tsystems.javaschool.model.entity.Patient;
-import com.tsystems.javaschool.model.entity.Treatment;
-import com.tsystems.javaschool.model.entity.UserEntity;
-import com.tsystems.javaschool.model.entity.enums.Role;
+import com.tsystems.javaschool.service.api.MapperService;
 import com.tsystems.javaschool.service.api.PatientService;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
-public class PatientServiceImpl extends AbstractServiceImpl<Patient, PatientRepository, PatientDto, Integer> implements PatientService {
+public class PatientServiceImpl implements PatientService {
+
+    private final PatientRepository dao;
+    private final MapperService mapper;
 
     @Autowired
-    public PatientServiceImpl(PatientRepository dao, ModelMapper mapper) {
-        super(dao, mapper, PatientDto.class, Patient.class);
+    public PatientServiceImpl(PatientRepository dao, MapperService mapper) {
+        this.dao = dao;
+        this.mapper = mapper;
     }
 
     @Override
-    public PatientDto convertToDTO(Patient patientEntity) {
-        int id = patientEntity.getId();
-
-        UserDto user = new UserDto();
-        user.setSurname(patientEntity.getUser().getSurname());
-        user.setFirstName(patientEntity.getUser().getFirstName());
-        user.setMiddleName(patientEntity.getUser().getMiddleName());
-        user.setDbirth(String.valueOf(patientEntity.getUser().getDbirth()));
-        user.setGender(patientEntity.getUser().getGender());
-        user.setAddress(patientEntity.getUser().getAddress());
-        user.setEmail(patientEntity.getUser().getEmail());
-        user.setFullName(new StringBuilder().append(patientEntity.getUser().getSurname()).append(" ").append(patientEntity.getUser().getFirstName().substring(0, 1).toUpperCase()).append(". ").append(patientEntity.getUser().getMiddleName().substring(0, 1).toUpperCase()).append(".").toString());
-
-        List<InsuranceDto> insuranceDtos = patientEntity.getInsurances().stream().map(this::convertInsuranceToDto).collect(Collectors.toList());
-
-        List<TreatmentDto> treatmentDtos = patientEntity.getTreatments().stream().map(this::convertTreatmentToDto).collect(Collectors.toList());
-
-        PatientDto patientDto = new PatientDto();
-        patientDto.setUserDto(user);
-        patientDto.setId(id);
-        patientDto.setInsurances(insuranceDtos);
-        patientDto.setTreatments(treatmentDtos);
-
-        return patientDto;
-    }
-
-    private InsuranceDto convertInsuranceToDto(Insurance insurance) {
-        InsuranceDto insuranceDto = new InsuranceDto();
-        insuranceDto.setInsuranceName(insurance.getName());
-        insuranceDto.setInsuranceNumber(insurance.getNumber());
-        insuranceDto.setStartDate(insurance.getStartDate());
-        insuranceDto.setEndDate(insurance.getEndDate());
-        return insuranceDto;
-    }
-
-    private TreatmentDto convertTreatmentToDto(Treatment treatment) {
-        TreatmentDto treatmentDto = new TreatmentDto();
-        treatmentDto.setDiagnosis(treatment.getDiagnosis());
-        treatmentDto.setStartDate(String.valueOf(treatment.getStartDate()));
-        treatmentDto.setEndDate(String.valueOf(treatment.getEndDate()));
-        treatmentDto.setStatus(String.valueOf(treatment.isStatus()));
-        return treatmentDto;
+    @Transactional
+    public List<PatientDto> findAll() {
+        return dao.findAll().stream().map(patient -> mapper.convertToDto(patient)).collect(Collectors.toList());
     }
 
     @Override
-    public List<PatientDto> convertToDTO(List<Patient> entities) {
-        return entities.stream().map(this::convertToDTO).collect(Collectors.toList());
+    @Transactional
+    public PatientDto findById(int id) {
+        return mapper.convertToDto(dao.findById(id));
     }
 
     @Override
-    public Patient convertToEntity(PatientDto dto) {
-
-        Patient patient = new Patient();
-
-        UserEntity user = new UserEntity();
-        user.setFirstName(dto.getUserDto().getFirstName());
-        user.setSurname(dto.getUserDto().getSurname());
-        user.setMiddleName(dto.getUserDto().getMiddleName());
-        user.setGender(dto.getUserDto().getGender());
-        user.setAddress(dto.getUserDto().getAddress());
-        user.setEmail(dto.getUserDto().getEmail());
-        user.setRole(Role.valueOf("OTHER"));
-        user.setDbirth(LocalDate.parse(dto.getUserDto().getDbirth()));
-
-        patient.setUser(user);
-
-        return patient;
+    public void save(PatientDto patientDto) {
+        dao.save(mapper.convertToEntity(patientDto));
     }
-
 }
