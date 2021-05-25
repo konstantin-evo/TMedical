e05b3debd482623852594370e9b6a9c61ffbb1ca
kpriluch <Konstantin.Priluchnyi@telekom.com>
2021-05-25 17:16:35 +0300

    Add TherapyCaseDto.java The build is deployment

---
+++ src/main/java/com/tsystems/javaschool/model/dto/TherapyCaseDto.java
@@ -0,0 +1,12 @@
+package com.tsystems.javaschool.model.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class TherapyCaseDto {
+    String date;
+    String time;
+    String status;
+}
