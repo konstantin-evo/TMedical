af055a2e16f936ce7cafc6bcd2ffe5bcc0411f0c
kpriluch <Konstantin.Priluchnyi@telekom.com>
2021-05-26 12:37:53 +0300

    Refactor "TreatmentType" enum (add displayValue method) The build is deployment

--- src/main/java/com/tsystems/javaschool/model/entity/enums/TreatmentType.java
+++ src/main/java/com/tsystems/javaschool/model/entity/enums/TreatmentType.java
@@ -1,6 +1,16 @@
 package com.tsystems.javaschool.model.entity.enums;
 
 public enum TreatmentType {
-    PROCEDURE,
-    MEDICINE
+    PROCEDURE("Procedure"),
+    MEDICINE("Medicine");
+
+    private final String displayValue;
+
+    private TreatmentType(String displayValue) {
+        this.displayValue = displayValue;
+    }
+
+    public String getDisplayValue() {
+        return displayValue;
+    }
 }
