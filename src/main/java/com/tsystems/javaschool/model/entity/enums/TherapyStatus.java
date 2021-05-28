a0dd0cc53aa03a125ad0b651cc3cc564908744bc
kpriluch <Konstantin.Priluchnyi@telekom.com>
2021-05-28 16:39:15 +0300

    Add "TherapyService" method to TherapyStatus enum The build is deployment

--- src/main/java/com/tsystems/javaschool/model/entity/enums/TherapyStatus.java
+++ src/main/java/com/tsystems/javaschool/model/entity/enums/TherapyStatus.java
@@ -1,7 +1,17 @@
 package com.tsystems.javaschool.model.entity.enums;
 
 public enum TherapyStatus {
-    PLANNED,
-    DONE,
-    CANCELED
+    PLANNED ("Planned"),
+    DONE ("Done"),
+    CANCELED ("Canceled");
+
+    private final String displayValue;
+
+    private TherapyStatus(String displayValue) {
+        this.displayValue = displayValue;
+    }
+
+    public String getDisplayValue() {
+        return displayValue;
+    }
 }
