ebcb917ee45a8341060c969cff7bb1aa0673cc58
kpriluch <Konstantin.Priluchnyi@telekom.com>
2021-05-31 19:45:09 +0300

    Add patient, medical, doctor variables in TherapyCaseDto The build is deployment

--- src/main/java/com/tsystems/javaschool/model/dto/TherapyCaseDto.java
+++ src/main/java/com/tsystems/javaschool/model/dto/TherapyCaseDto.java
@@ -6,6 +6,10 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 public class TherapyCaseDto {
+    int id;
+    String patient;
+    String medical;
+    String doctor;
     String date;
     String time;
     String status;
