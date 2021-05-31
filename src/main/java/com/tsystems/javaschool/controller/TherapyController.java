a7a51e01f67a606a527280476b761c35bd174df1
kpriluch <Konstantin.Priluchnyi@telekom.com>
2021-05-31 19:45:34 +0300

    Add @PostMapping("/update/{id}") in TherapyController.java The build is deployment

--- src/main/java/com/tsystems/javaschool/controller/TherapyController.java
+++ src/main/java/com/tsystems/javaschool/controller/TherapyController.java
@@ -1,15 +1,13 @@
 package com.tsystems.javaschool.controller;
 
 import com.tsystems.javaschool.model.dto.TherapyCaseDto;
-import com.tsystems.javaschool.service.api.PatientService;
 import com.tsystems.javaschool.service.api.TherapyService;
-import com.tsystems.javaschool.service.api.TreatmentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,7 +26,16 @@ public class TherapyController {
     public String therapiesByDay(@PathVariable("day") String day, Model model){
         List<TherapyCaseDto> therapyCases = therapyService.findCasesByDay(day);
         model.addAttribute("therapyCases", therapyCases);
+        model.addAttribute("day", day);
         return "therapy/therapy-day";
+    }
+
+    @PostMapping("/update/{id}")
+    public String updateStatus(@PathVariable("id") int id, @ModelAttribute("casePost") TherapyCaseDto therapyCaseDto) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String email =  authentication.getName();
+        therapyService.setStatus(id, email, "DONE");
+        return "redirect:/home";
     }
 
 }
