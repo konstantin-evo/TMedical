5bd92a3ae1605a59007fe383a937f60cd254dd88
kpriluch <Konstantin.Priluchnyi@telekom.com>
2021-06-02 12:49:46 +0300

    Add Serializable to InsuranceDto.java The build is deployment

--- src/main/java/com/tsystems/javaschool/model/dto/InsuranceDto.java
+++ src/main/java/com/tsystems/javaschool/model/dto/InsuranceDto.java
@@ -3,11 +3,12 @@ package com.tsystems.javaschool.model.dto;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 
 @Data
 @NoArgsConstructor
-public class InsuranceDto {
+public class InsuranceDto implements Serializable {
     private String insuranceName;
     private String insuranceNumber;
     private LocalDate startDate;
