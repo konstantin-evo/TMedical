efa95dae13fea75cdade932a79ce3234ed351544
kpriluch <Konstantin.Priluchnyi@telekom.com>
2021-06-02 12:49:25 +0300

    Add Serializable to DaysWrapper.java The build is deployment

--- src/main/java/com/tsystems/javaschool/model/dto/DaysWrapper.java
+++ src/main/java/com/tsystems/javaschool/model/dto/DaysWrapper.java
@@ -3,11 +3,12 @@ package com.tsystems.javaschool.model.dto;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.util.List;
 
 @Data
 @NoArgsConstructor
-public class DaysWrapper {
+public class DaysWrapper implements Serializable {
     private  List<TherapyDaysDto> days;
 
     public void addDay(TherapyDaysDto day) {
