941a1f9a9b7db462520a4c199d59e08517ef9125
kpriluch <Konstantin.Priluchnyi@telekom.com>
2021-06-09 15:02:36 +0300

    Add "deleteTherapy" mapping to TherapyController The build is deployment

--- src/main/java/com/tsystems/javaschool/controller/TherapyController.java
+++ src/main/java/com/tsystems/javaschool/controller/TherapyController.java
@@ -1,7 +1,9 @@
 package com.tsystems.javaschool.controller;
 
+import com.tsystems.javaschool.controller.exception.ExceptionTreatmentNotFound;
 import com.tsystems.javaschool.model.dto.TherapyCaseDto;
 import com.tsystems.javaschool.service.api.TherapyService;
+import com.tsystems.javaschool.service.api.TreatmentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -9,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Controller
@@ -16,10 +19,12 @@ import java.util.List;
 public class TherapyController {
 
     private final TherapyService therapyService;
+    private final TreatmentService treatmentService;
 
     @Autowired
-    public TherapyController(TherapyService therapyService) {
+    public TherapyController(TherapyService therapyService, TreatmentService treatmentService) {
         this.therapyService = therapyService;
+        this.treatmentService = treatmentService;
     }
 
     @GetMapping("/{day}")
@@ -31,11 +36,23 @@ public class TherapyController {
     }
 
     @PostMapping("/update/{id}")
-    public String updateStatus(@PathVariable("id") int id) {
+    public String updateStatus(@PathVariable("id") int id) throws ExceptionTreatmentNotFound {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         String email =  authentication.getName();
         therapyService.setStatus(id, email, "DONE");
-        return "redirect:/homePage";
+        therapyService.sendMessageByDay(therapyService.findCaseById(id).getDate());
+        String treatmentId = String.valueOf(treatmentService.findByTherapyId(id).getId());
+        return "redirect:/treatment/"+treatmentId;
+    }
+
+    @PostMapping("/delete/{id}")
+    public String deleteTherapy(@PathVariable("id") int id) throws ExceptionTreatmentNotFound {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String email =  authentication.getName();
+        String treatmentId = String.valueOf(treatmentService.findByTherapyId(id).getId());
+        therapyService.deleteTherapy(id, email);
+        therapyService.sendMessageByDay(String.valueOf(LocalDate.now()));
+        return "redirect:/treatment/"+treatmentId;
     }
 
 }
