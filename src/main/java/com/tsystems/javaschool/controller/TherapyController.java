22c960590433d130cfecab596313b24ae5d27b4a
kpriluch <Konstantin.Priluchnyi@telekom.com>
2021-06-16 15:41:12 +0300

    Delete unnecessary import from TherapyController The build is deployment

--- src/main/java/com/tsystems/javaschool/controller/TherapyController.java
+++ src/main/java/com/tsystems/javaschool/controller/TherapyController.java
@@ -12,7 +12,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
 import java.util.List;
 
 @Controller
